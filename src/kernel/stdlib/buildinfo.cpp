d950c2983b915a9ae606517f765c10108747fdc2
Matthew Fosdick <matthew.fosdick@gmail.com>
2016-06-06 21:48:14 -0700

    .gitignore fixed

--- src/kernel/stdlib/buildinfo.cpp
+++
@@ -1,8 +0,0 @@
-#pragma GCC diagnostic ignored "-Wunused-variable"
-const char *BUILD_REV="fa544877348ef62f8fa74bf49834dce822873d4d";
-const char *BUILD_BRANCH="master";
-const bool  BUILD_DEV=false;
-const char *BUILD_NAME="Rutherfordium Dichromate";
-const char *BUILD_FORMULA="Rf(Cr₂O₇)₄";
-const char *COMPILER="clang";
-const char *COMPILER_VER="3.8.0";
