d64b09c1519c0376c1e7806b8f962fc53add881a
Matthew Fosdick <matthew.fosdick@gmail.com>
2016-07-09 15:42:37 -0700

    making progress

--- src/libkpp/include/_memory/shared_ptr.hpp
+++ src/libkpp/include/_memory/shared_ptr.hpp
@@ -21,8 +21,119 @@
 #include <type_traits>
 namespace std {
 template <class T, class D = default_delete<nullptr_t>> class weak_ptr;
-template <class T, class D = default_delete<nullptr_t>> class shared_ptr;
-template <class T, class D> class weak_ptr {
+template <class T, class D = default_delete<nullptr_t>> class shared_ptr {
+  public:
+	typedef typename details::control_block<T, D>::element_type element_type;
+	typedef weak_ptr<T, D> weak_type;
+
+  private:
+	element_type *pointer;
+	details::control_block<T, D> *control;
+
+  public:
+	void reset() {
+		control->release();
+		if(control->can_delete()) { delete control; }
+		pointer(nullptr);
+		control(details::nullptrctrl<D>);
+	}
+	// constructors
+	constexpr shared_ptr() : pointer(nullptr), control(details::nullptrctrl<D>) {}
+	constexpr shared_ptr(std::nullptr_t) : pointer(nullptr), control(details::nullptrctrl<D>) {}
+	template <class Y> explicit shared_ptr(Y *ptr) : pointer(nullptr) {
+		control = new details::control_block<Y, D>{ptr};
+		control->grab();
+	}
+	template <class Y, class Del> explicit shared_ptr(Y *ptr) : pointer(nullptr) {
+		control = new details::control_block<Y, Del>{ptr};
+		control->grab();
+	}
+	template <class Del> explicit shared_ptr(std::nullptr_t) : pointer(nullptr) {
+		control = details::nullptrctrl<D>;
+		control->grab();
+	}
+	template <class Y> shared_ptr(const shared_ptr<Y, D> &r, element_type *ptr) : pointer(ptr), control(r.control) {
+		control->grab();
+	}
+	shared_ptr(const shared_ptr &r) : pointer(r.pointer), control(r.control) { control->grab(); }
+	template <class Y> shared_ptr(const shared_ptr<Y, D> &r) : pointer(r.pointer), control(r.control) {
+		control->grab();
+	}
+	// maybe need a <class Y, class Del> overload here
+	shared_ptr(shared_ptr &&r) {
+		pointer(r.pointer);
+		control(r.control);
+		control->grab();
+		r.reset();
+	}
+	template <class Y> shared_ptr(shared_ptr<Y, D> &&r) {
+		pointer(r.pointer);
+		control(r.control);
+		control->grab();
+		r.reset();
+	}
+	template <class Y> explicit shared_ptr(const std::weak_ptr<Y, D> &r); // IMP below
+	template <class Y, class Del> shared_ptr(std::unique_ptr<Y, Del> &&r) {
+		control = new details::control_block<Y, Del>{r.release(), r.get_deleter()};
+		control->grab();
+	}
+	// deconstructors
+	~shared_ptr() { reset(); }
+	// operator='s
+	shared_ptr &operator=(const shared_ptr &r) {
+		reset();
+		pointer(r.pointer);
+		control(r.control);
+		control->grab();
+		return *this;
+	}
+	template <class Y> shared_ptr &operator=(const shared_ptr<Y, D> &r) {
+		reset();
+		pointer(r.pointer);
+		control(r.control);
+		control->grab();
+	}
+	shared_ptr &operator=(const shared_ptr &&r) {
+		reset();
+		pointer(r.pointer);
+		control(r.control);
+		control->grab();
+		r.reset();
+		return *this;
+	}
+	template <class Y> shared_ptr &operator=(const shared_ptr<Y, D> &&r) {
+		reset();
+		pointer(r.pointer);
+		control(r.control);
+		control->grab();
+		r.reset();
+		return *this;
+	}
+	template <class Y, class Del> shared_ptr &operator=(std::unique_ptr<Y, Del> &&r) {
+		reset();
+		control = new details::control_block<Y, Del>{r.release(), r.get_deleter()};
+		control->grab();
+		return *this;
+	}
+	void swap(shared_ptr &r) {
+		element_type *ptr(pointer);
+		details::control_block<T, D> *ctrl(control);
+		pointer = r.pointer;
+		control = r.control;
+		r.pointer = ptr;
+		r.control = ctrl;
+	}
+	element_type *get() const { return pointer ? pointer : control->pointer; }
+	std::enable_if<!std::is_array<T>::value, T &> operator*() const { return pointer ? *pointer : *control->pointer; }
+	std::enable_if<!std::is_array<T>::value, T *> operator->() const { return pointer ? *pointer : *control->pointer; }
+	std::enable_if<std::is_array<T>::value, element_type &> operator[](std::ptrdiff_t idx) {
+		return pointer ? pointer[idx] : control->pointer[idx];
+	}
+	long use_count() const { return control->shared_count; }
+	bool unique() const { return use_count() == 1; }
+	explicit operator bool() const { return !!pointer || !!control->shared_count; }
+};
+template <class T, class D> class weak_ptr : private shared_ptr<T, D> {
   public:
 	typedef typename details::control_block<T, D>::element_type element_type;
 
@@ -48,12 +159,10 @@ template <class T, class D> class weak_ptr {
 	template <class Y> weak_ptr(weak_ptr<Y> &&r) : pointer(r.pointer), control(r.control) { r.reset(); }
 	template <class Y, class E> weak_ptr(weak_ptr<Y, E> &&r) : pointer(r.pointer), control(r.control) { r.reset(); }
 	void reset() {
-		pointer = nullptr;
 		control->release_weak();
-		if(control->can_delete) {
-			delete control;
-			control = details::nullptrctrl<D>;
-		}
+		if(control->can_delete) { delete control; }
+		pointer(nullptr);
+		control(details::nullptrctrl<D>);
 	}
 	~weak_ptr() { reset(); }
 	weak_ptr &operator=(const weak_ptr &r) {
@@ -123,6 +232,5 @@ template <class T, class D> class weak_ptr {
 	long use_count() const { return control->shared_count; }
 	bool expired() const { return use_count() == 0; }
 };
-template <class T, class D> class shared_ptr {};
 }
 #endif
